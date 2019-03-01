d7ea41abc10600225943aee730e6f037301af863
Gary Tempus <gary@heysparkbox.com>
2019-03-01 08:02:13 -0500

    style: Fix linting errors in sudoku-validator.

--- test/sudoku-validator.test.js
+++ test/sudoku-validator.test.js
@@ -1,31 +1,27 @@
 const { expect } = require('chai');
 const SudokuValidator = require('../src/sudoku-validator');
+const Board = require('../src/board');
 
 describe('Sudoku Validator', () => {
   it('returns false if the board is invalid', () => {
-    const emptyBoard = [];
+    const invalidBoard = new Board([]);
     const rulesStub = {
-      areSatisfied: (board) => false,
+      areSatisfied: board => false,
     };
 
-    const validator = new SudokuValidator(emptyBoard, rulesStub);
+    const validator = new SudokuValidator(invalidBoard, rulesStub);
 
     const result = validator.valid();
     expect(result, 'This board should not be valid').to.be.false;
   });
 
   it('returns true if the board is valid', () => {
-    const simpleBoard = [
-      [ 1, 2, 3, 4 ],
-      [ 3, 4, 1, 2 ],
-      [ 2, 3, 4, 1 ],
-      [ 4, 1, 2, 3 ],
-    ];
+    const validBoard = new Board([[1]]);
     const rulesStub = {
-      areSatisfied: (board) => true,
+      areSatisfied: board => true,
     };
 
-    const validator = new SudokuValidator(simpleBoard, rulesStub);
+    const validator = new SudokuValidator(validBoard, rulesStub);
     const result = validator.valid();
     expect(result, 'This board should be valid').to.be.true;
   });
